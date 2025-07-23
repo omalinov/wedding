cebf27edb57e7c3cc8be3ef0a22e83bf3169d531
omalinov <ognyan.m@coherent-labs.com>
2025-07-23 14:16:23 +0300

    Add a bunch of stupid stuff

---
+++ createNavbar.js
@@ -0,0 +1,17 @@
+document.body.insertAdjacentHTML("afterbegin",
+    `<nav class="readable-font">
+        <ul>
+            <li><a href="#our-story">Our Story</a></li>
+            <li><a href="event-details.html">Event Details</a></li>
+            <li><a href="travel.html">Travel & Accommodation</a></li>
+            <li><a href="rsvp.html">RSVP</a></li>
+            <li><a href="#registry">Registry</a></li>
+            <li class="logo"><a href="index.html"><img src="./img/initials.png" alt="Logo"></a></li>
+            <li><a href="#wedding-party">Wedding Party</a></li>
+            <li><a href="#gallery">Photos & Videos</a></li>
+            <li><a href="#faq">FAQ</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+            <li><a href="#social">Social Media</a></li>
+        </ul>
+    </nav>`
+);
